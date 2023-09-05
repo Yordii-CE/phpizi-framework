92fc83ac4507656e7fe8b21f47d07104249fc62a
Yordii <yokdy360@email.com>
2023-09-05 11:07:38 -0500

    Autoload Refactoring

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-require_once 'autoload.php';
-
 //GLOBALS FUNCTIONS FOR APP
-require_once 'framework/global_funcs/json.php';
-require_once 'framework/global_funcs/redirectToAction.php';
-require_once 'framework/global_funcs/redirectToUrl.php';
-require_once 'framework/global_funcs/render.php';
-require_once 'framework/global_funcs/to.php';
-require_once 'framework/global_funcs/view.php';
+require_once __DIR__ . '/global_funcs/json.php';
+require_once __DIR__ . '/global_funcs/redirectToAction.php';
+require_once __DIR__ . '/global_funcs/redirectToUrl.php';
+require_once __DIR__ . '/global_funcs/render.php';
+require_once __DIR__ . '/global_funcs/to.php';
+require_once __DIR__ . '/global_funcs/view.php';
 
 
 require_once 'app/main.php';
