5137c583417d69c4f3d53cf466c0a60442932bc3
Yordii <yokdy360@email.com>
2023-09-25 22:37:13 -0500

    Fix global include_file

--- global_funcs/include_file.php
+++ global_funcs/include_file.php
@@ -1,12 +1,18 @@
 <?php
 
+use Framework\Response\View;
 use Framework\Utils\Routing\Path;
 
-function INCLUDE_FILE($fileName, $vars = [])
+function INCLUDE_FILE($filePath, $vars = [])
 {
     //INCLUDE_FILE() SOLO DEBEJE EJECUTARSE EN VISTAS / TEMPLATE
-    if (!file_exists(Path::getPathView($fileName))) throw new Exception("ERROR REQUIRE FILE: not found");
+
+    extract(View::$vars);
+
+    $route = Path::getPathView($THIS) . '/' . $filePath;
+
+    if (!file_exists($route)) throw new Exception("ERROR REQUIRE FILE: not found");
 
     extract($vars);
-    include Path::getPathView($fileName);
+    include $route;
 }
